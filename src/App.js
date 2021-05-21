88dd3fd4b72adb7a0862f9e8c87dc23f9ffc5054
Nirali <bhalodiyanirali241@gmail.com>
2021-05-21 10:44:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ function App() {
     <Router>
     <Switch>
           <Route exact path="/"><Contact /></Route>
-          <div className="bg-info " style={{marginLeft: "20%" , width: '80%', height: "100vh"}} data-aos="fade-left" data-aos-offset="900"><Route exact path='/Signup'><Signup /></Route></div>
+          <div className="bg-info " style={{marginLeft: "20%" , width: '80%', height: "100vh"}} data-aos="fade-up"
+     data-aos-easing="linear"
+     data-aos-duration="1100"><Route exact path='/Signup'><Signup /></Route></div>
     </Switch>
     </Router>
 
